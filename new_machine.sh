9a6e4035f7468d09daeb38271cdd90444792b13d
Dennis Zander <denniszander15@web.de>
2024-01-14 11:01:57 +0100

    Copy config files for new machine

--- new_machine.sh
+++ new_machine.sh
@@ -1,6 +1,19 @@
 #!bin/bash
+
+# Make sure you have installed the following packages
+# bash, vim, i3, tmux, feh, picom, alacritty, and lf
+
+# Create config files for multi use configs
+cp ~/.config/bash/.bashrc_all ~/.config/bash/.bashrc
+cp ~/.config/vim/.vimrc_all ~/.config/vim/.vimrc
+cp ~/.config/i3/config_all ~/.config/i3/config
+cp ~/.config/lf/lfrc_all ~/.config/lf/lfrc
+
 # Create symbloc links 
 ln -s ~/.config/vim/.vimrc ~/.vimrc
 ln -s ~/.config/bash/.bashrc ~/.bashrc 
+
+
+
 
 
