f8cf9ea4e4c49ebb9ee60a27992d0b376c4dccfa
Dennis Zander <denniszander15@web.de>
2024-02-04 23:39:49 +0100

    Much tmux related stuff

---
+++ tmux/plugins/tmux-fzf-session/tmux-switch-pane.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# customizable
+LIST_DATA="#{window_name} #{pane_title} #{pane_current_path} #{pane_current_command}"
+FZF_COMMAND="fzf-tmux -p --delimiter=: --with-nth 4 --color=hl:2"
+
+# do not change
+TARGET_SPEC="#{session_name}:#{window_id}:#{pane_id}:"
+
+# select pane
+LINE=$(tmux list-panes -a -F "$TARGET_SPEC $LIST_DATA" | $FZF_COMMAND) || exit 0
+# split the result
+args=(${LINE//:/ })
+# activate session/window/pane
+if [ -n "$TMUX" ]; then
+	tmux select-pane -t ${args[2]} && tmux select-window -t ${args[1]} && tmux switch-client -t ${args[0]}
+else
+	tmux a -t ${args[0]} && tmux select-window -t ${args[1]} && tmux select-pane -t ${args[2]}
+fi
+
+
